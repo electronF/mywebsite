b834d44c9efadc2df7c05414aeea0729d3f19568
electronF <bertrandovide@gmail.com>
2023-03-07 20:31:56 +0100

    update

--- webapp/frontend/views/uix/user/editable_dropdown_button/editable_dropdown_button.ts
+++ webapp/frontend/views/uix/user/editable_dropdown_button/editable_dropdown_button.ts
@@ -1,7 +1,57 @@
-$(".editable-dropdown-button .field button").on("click", function(event){
-    var list_item = $(event.currentTarget).parent().next()
-    console.log(list_item)
-    var button = $(list_item).children("button")
-    console.log(button)
-    button.click()
-})
+class EditableDropdownButton
+{
+
+    #value:string=''
+
+    #eventListenner()
+    {
+        $(".editable-dropdown-button .field button").on("click", function(event){
+            var dropdown = event.currentTarget.parentElement.parentElement as HTMLDivElement
+            var list_item = dropdown.getElementsByClassName('list-item') as HTMLCollectionOf<HTMLDivElement>
+
+            if(list_item.length > 0)
+            {
+                if($(list_item[0]).css('display').trim().toLowerCase() === 'none' )
+                {
+                    $('.editable-dropdown-button .dropdown-menu').css({'display': 'none'})
+                    $(list_item[0]).css({'display': 'flex'})
+                }
+                else
+                {
+                    $(list_item[0]).css({'display': 'none'})
+                }
+            }
+
+        })
+
+
+        document.addEventListener('click', function(event){
+            var element = event.target as HTMLElement
+
+            if(element.tagName.trim().toLowerCase() === 'img')
+            {
+                element = element.parentElement
+            }
+
+            var condition = (
+                (['input', 'button']).includes(element.tagName.trim().toLowerCase())
+                && element.parentElement.parentElement.classList.contains("editable-dropdown-button"))
+
+            if(!condition)
+            {
+                $('.editable-dropdown-button .dropdown-menu').css({'display': 'none'})
+            }
+        })
+
+        document.readyState
+    }
+
+    listen()
+    {
+        this.#eventListenner()
+    }
+}
+
+
+(new EditableDropdownButton()).listen()
+
